047f53b98c0ff11092e8242c1f3183459048fc24
caleb jephuneh <calebjephunneh@gmail.com>
2023-04-15 17:06:22 +0300

    added tests

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    text = models.TextField()
+
+    def __str__(self): # new
+        return self.text[:50]
+    
+    
